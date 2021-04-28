beb1e869ddff31e1d2760fbca78e2dd68dd1de4e
Martin Yanchev <myanch200@gmail.com>
2021-04-28 21:49:44 +0100

    Adding the initial page for acconts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+def profile(request):
+    check = "works"
+    return render(request, 'accounts/profile.html',{'check':check})
