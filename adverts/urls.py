ae818441351da14e2adee3a28cfc476eda705527
Martin Yanchev <myanch200@gmail.com>
2021-07-21 21:39:41 +0100

    [Update] Adding add to wishlist functionality

--- adverts/urls.py
+++ adverts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import advert_details,wishlist_page
+from .views import advert_details,wishlist_page,remove_from_wishlist,add_to_wishlist
 
 app_name = "adverts"
 
 urlpatterns = [
     path('details/<int:id>/',advert_details,name="advert_details"),
-    path('wishlist',wishlist_page,name="wishlist_page")
+    path('wishlist',wishlist_page,name="wishlist_page"),
+    path('wishlist-remove/<int:pk>/',remove_from_wishlist,name='remove_from_wishlist'),
+    path('add_to_wishlist/<int:pk>/',add_to_wishlist, name='add_to_wishlist')
 
     
 
--- adverts/views.py
+++ adverts/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Advert,AdvertImage, WishList
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def advert_details(request,id):
     advert = Advert.objects.get(id = id)
     advert_images = AdvertImage.objects.filter(advert=advert)
     context = {"advert":advert,"advert_images":advert_images}
     return render(request,"adverts/advert_detail.html",context)
-
+@login_required(login_url='accounts:login')
 def wishlist_page(request):
     wishlist = WishList.objects.get(user = request.user)
     for item in wishlist.adverts.all():
         print(item.title)
     context = {'wishlist':wishlist}
     return render(request,'adverts/wishlist_page.html',context)
+
+
+@login_required(login_url='accounts:login')
+def remove_from_wishlist(request,pk):
+    wishlist = request.user.wishlist
+    item = wishlist.adverts.get(id = pk)
+    wishlist.adverts.remove(item)
+    return redirect('adverts:wishlist_page')
+@login_required(login_url='accounts:login')
+def add_to_wishlist(request,pk):
+    item = Advert.objects.get(id = pk)
+    wishlist = request.user.wishlist
+    wishlist.adverts.add(item)
+    return redirect('adverts:wishlist_page')
