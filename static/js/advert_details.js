71e9a0b68ea25956dffa7e2b703cde5c343d50b9
Martin Yanchev <myanch200@gmail.com>
2021-07-27 21:25:36 +0100

    [Update] Separating the slider functionality

--- static/js/advert_details.js
+++ static/js/advert_details.js
@@ -1,26 +1,14 @@
 import {toggleWishlist} from "./wishlist_utils.js"
 
-const images = document.getElementsByClassName('img-item');
-const thumbail = document.getElementById("thumbnailImg");
-const thumbnailWrapper = document.getElementsByClassName('thumbnail-wrapper')[0];
-const leftArrow = document.getElementById("leftArrow");
-const rightArrow = document.getElementById("rightArrow");
+
 const callButton = document.getElementsByClassName('call-button')[0];
 const wishlistToggleBtn = document.getElementById("toggleWishlistBtn");
 
-let counter = 0 ;
-let index =0;
+
 thumbnailWrapper.disabled = true;
 //Event Listeners
 
-[leftArrow,rightArrow].forEach(item =>{
-    item.addEventListener("click",changeImage)
-})
 
-//Adding event listener for each of the images
-for( let j = 0 ; j < images.length;j++){
-    images[j].addEventListener('click',selectImage)
-}
 
 //Event listener for the call button
 callButton.addEventListener('click',showPhoneNumber);
@@ -41,53 +29,3 @@ function showPhoneNumber(event){
 }
 
 //On click gets the target src and pass it to the setImage function
-function selectImage(event){
-
-    setImage(event.target.src)
-}
-
-/*
-    I created setImage function rather that invoking setAttribute in both selectImage and Change image
-    In this way we don't need to know which function is called all we do is set the src attribute to be the path passed as parameter.
-*/
-function setImage(path){
-    
-    thumbail.setAttribute('src',path);
-    thumbail.classList= 'fade-in';
-    //Remove the animation class once the animation is completed 
-    thumbail.onanimationend = function(){
-        thumbail.classList = '';
-    }
-}
-
-
-
-/* 
-    Change image takes and event for paramater 
-    and based on which arrow is pressed increments or decrements the counter variable.
-    Which then we use to acces a image from the images array.
-    I manage to cover some of the edge cases , but have a lot to test ,yet.
-*/
-
-
-function changeImage(event){
-
-        if(event.target === rightArrow){
-            counter++;
-            counter = counter % images.length;
-        }
-
-        if(event.target === leftArrow){
-            counter--;
-            
-            if(counter < 0 ){
-                counter = images.length -1
-            }           
-        }
-        index = counter ;
-        setImage(images[index].src)
-
-   
-
-
-}
---
+++ static/js/slider.js
@@ -0,0 +1,66 @@
+const images = document.getElementsByClassName('img-item');
+const thumbail = document.getElementById("thumbnailImg");
+const thumbnailWrapper = document.getElementsByClassName('thumbnail-wrapper')[0];
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+
+[leftArrow,rightArrow].forEach(item =>{
+    item.addEventListener("click",changeImage)
+})
+
+//Adding event listener for each of the images
+for( let j = 0 ; j < images.length;j++){
+    images[j].addEventListener('click',selectImage)
+}
+let counter = 0 ;
+let index =0;
+export function selectImage(event){
+
+    setImage(event.target.src)
+}
+
+/*
+    I created setImage function rather that invoking setAttribute in both selectImage and Change image
+    In this way we don't need to know which function is called all we do is set the src attribute to be the path passed as parameter.
+*/
+ function setImage(path){
+    
+    thumbail.setAttribute('src',path);
+    thumbail.classList= 'fade-in';
+    //Remove the animation class once the animation is completed 
+    thumbail.onanimationend = function(){
+        thumbail.classList = '';
+    }
+}
+
+
+
+/* 
+    Change image takes and event for paramater 
+    and based on which arrow is pressed increments or decrements the counter variable.
+    Which then we use to acces a image from the images array.
+    I manage to cover some of the edge cases , but have a lot to test ,yet.
+*/
+
+
+export function changeImage(event){
+
+        if(event.target === rightArrow){
+            counter++;
+            counter = counter % images.length;
+        }
+
+        if(event.target === leftArrow){
+            counter--;
+            
+            if(counter < 0 ){
+                counter = images.length -1
+            }           
+        }
+        index = counter ;
+        setImage(images[index].src)
+
+   
+
+
+}
