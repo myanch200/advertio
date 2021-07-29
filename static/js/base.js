564afeb0808293a0d8c22c2f1fbc0b4dd1ba1bcd
Martin Yanchev <myanch200@gmail.com>
2021-07-29 13:31:55 +0100

    [Fix]Event listesteners not adding for new ads

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,10 @@
 
 import {toggleWishlist} from "./wishlist_utils.js"
-let toggleWishlistButtons = document.getElementsByClassName('toggle-wishlist');
-//Event listener for each button
-for(let i = 0; i <= toggleWishlist.length;i++){
-    toggleWishlistButtons[i].addEventListener("click",toggleWishlist);
+let toggleWishlistButtons = document.querySelectorAll('.toggle-wishlist');
+for (let item of toggleWishlistButtons) {
+    item.addEventListener("click",toggleWishlist)
 }
+
 
 
 
